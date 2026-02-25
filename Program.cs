ea62ec67aeab814fff5f3517a738c8f4f8e5b716
edenmoshailov-hash <eden.moshailov@rabinky.co.il>
2026-02-25 07:16:30 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp8
+namespace ConsoleApp9
 {
     internal class Program
     {
@@ -17,356 +18,278 @@ namespace ConsoleApp8
                 Console.Write(arr[i] + " ");
                 if (i == arr.Length - 1) Console.Write("}");
             }
+            Console.WriteLine();
         }
-        // static bool ispolindrom1(int[] arr)
-        // {
-        //    int left=0, right=arr.Length-1;
-        //     bool ispolindrom = true;
-        //     while(left < right && ispolindrom)
-        //     {
-        //         if (arr[left] != arr[right])
-        //             ispolindrom = false;
-        //         left++;
-        //         right--;
-        //     }
-        //     return ispolindrom;
-        // }
+        //מערך צוברים
 
-        //static void rotatearray(int[] arr)
-        // {
-        //     int tmp = arr[0];
-        //     for (int i = 1; i < arr.Length; i++)
-        //         arr[i - 1] = arr[i];
-
-        //     arr[arr.Length-1] = tmp;
-
-        // }
-        // static void copyarray()
-        // {
-        // int[] arr1 = { 1, 2, 3, 4, 5 }; //מערך מקור
-        // int[] arr2 = new int[arr1.Length]; //יצירת מערך חדש בגודל זהה למערך המקורי
-        //     // העתקת הערכים מהמערך המקורי למערך החדש
-        // for (int i = 0; i<arr1.Length; i++)
-        // {
-        //    arr2[i] = arr1[i];
-        // }
-        // }
-
-        // static void maximuminarray()
-        // {
-        // int[] arr = { 23, 28, 31 };
-
-        // int max = arr[0]; // משתנה לאחסון הערך המקסימלי
-
-        // for (int i = 1; i<arr.Length; i++)
-        // {
-        //  if (arr[i] > max)
+        //static double[] buildmilkarray(int numofcows)
+        //{
+        //    double[] arr = new double[numofcows];
+        //    for (int day = 1; day <= 5; day++)
+        //    {
+        //        for (int i = 1; i <=numofcows; i++)
         //        {
-        //       max = arr[i];
-        //       }
-        //     }
-        // }
+        //            Console.Write("day" + day + "cow" + i + ":");
+        //            arr[i-1]+=double.Parse(Console.ReadLine());
+        //        }
+        //    }
+        //    return arr;
+            //לא לשכוח בmain 
+            //int num = int.Parse(Console.ReadLine());
+            //double[] milk = buildmilkarray(num);
+            //printarray(milk);
+        //}
+
+        //static void Task1()
+        //{
+        //    Random rnd = new Random();
+        //    int[] counter=new int[6];
+        //    for (int i = 0;i < 20;i++)
+        //    {
+        //        int num=rnd.Next(1,7);
+        //        Console.Write(num + "  ");
+        //        counter[num-1]++;
+        //    }
+        //    printarray(counter);
+        //}
+
+
+        //static void task2()
+        //כמה פעמים כל אות מופיעה במערך
+        //{
+           
+        //    int[] arr = { 'a', 'b', 'c', 'a', 'b', 'a' };
+        //    int max=0;
+        //    int[] counter = new int[26];
+        //    for(int i = 0;i < arr.Length;i++)
+        //    {
+        //        counter[arr[i]-'a']++;
+        //    }
+        //    printarray(counter);
+        //    for(int i = 0; i < counter.Length;i++)
+        //    {
+        //        if (counter[i] > max)
+        //            max=i;
+        //    }
+        //    Console.Write((char)(max+'a'));
+        //}
 
 
 
-        // static void maxindex()
-        // {
-        //     int[] temperatures = { 23, 28, 31, 26, 30 };
-
-        //     // משתנים לאחסון הערך המקסימלי ואינדקסו
-        //     int max = temperatures[0];
-        //     int maxIndex = 0;
-
-        //     for (int i = 1; i < temperatures.Length; i++)
-        //     {
-        //         if (temperatures[i] > max)
-        //         {
-        //             max = temperatures[i];
-        //             maxIndex = i;
-        //         }
-        //     }
-
-        //     Console.WriteLine("The highest temperature is: " + max);
-        //     Console.WriteLine("It occurred on day number: " + (maxIndex + 1));
-        // }
-
-
-        // static void reversearr()
-        // {
-        //     int[] arr = { 1, 2, 3, 4, 5 }; //מערך מקור
-
-        //     int[] reversed = new int[arr.Length]; //יצירת מערך חדש בגודל זהה
-
-        //     for (int i = 0; i < arr.Length; i++)
-        //     {
-        //         reversed[i] = arr[arr.Length - 1 - i];
-        //     }
-        //     printarray(reversed);
-        // }
-
+        //last \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        //static void task1()
+        //{
+        //    Random rnd = new Random();
+        //    int count = 0;
+        //    for (int i = 0;i<=10;i++)
+        //    {
+        //       int num=rnd.Next(0,10);
+        //        Console.WriteLine(num);
+        //        if (num == 3) count++;
+        //    }
+        //    Console.WriteLine(count);
+        //}
 
         //static void task3()
+        //    //הורדת כפילויות
         //{
-        //    int[] arr;
-        //    int sum = 0;
-        //    double avg = 0;
-        //    while (true)
+        //    Random rnd = new Random();
+        //    int[] arr = new int[10];
+        //    int[] counter= new int[10];
+        //    int[] newarr = new int[arr.Length];
+        //    for (int i = 0;i < arr.Length;i++)
         //    {
-        //        Console.WriteLine("pick a number for the array,please enter a valid one");
-        //        int size = int.Parse(Console.ReadLine());
-        //        if (size > 0)
-        //        {
-        //            arr = new int[size];
-        //            break;
-        //        }
-        //        else Console.WriteLine("invalid,write again");
+        //        arr[i] = rnd.Next(0, 10);
         //    }
+        //    printarray(arr);
         //    for (int i = 0; i < arr.Length; i++)
         //    {
-        //        arr[i] = int.Parse(Console.ReadLine());
+        //        newarr[i] = arr[i];
         //    }
-        //    //sum
-        //    for (int i = 0; i < arr.Length; i++)
+        //        for (int i = 0; i < arr.Length; i++)
         //    {
-        //        sum += arr[i];
+        //        counter[arr[i]]++;
         //    }
-        //    //avg
-        //    avg = (double)sum / arr.Length;
-        //    //max
-        //    int max = arr[0];
-        //    int maxind = 0;
-        //    int min = arr[0];
-        //    int minind = 0;
-        //    for (int i = 1; i < arr.Length; i++)
+        //        printarray(counter);
+        //    for(int i = 0; i < arr.Length; i++)
         //    {
-        //        if (arr[i] > max)
-        //        {
-        //            max = arr[i];
-        //            maxind = i;
-        //        }
-
+        //        if (counter[arr[i]] > 1)
+        //            counter[arr[i]] = -99;
+        //        else if (counter[arr[i]] != 1)
+        //            newarr[i] = -1;
         //    }
-        //    for (int i = 1; i < arr.Length; i++)
-        //    {
-        //        if (arr[i] < min)
-        //        {
-        //            min = arr[i];
-        //            minind = i;
-        //        }
-        //    }
-        //    int[] counter = new int[max];
-        //    for (int i = 0; i < counter.Length; i++)
-        //          { 
-        //                counter[i]=0;
-        //            }
-        //            for(int i=0;i<counter.Length;i++)
-        //            {
-        //                counter[arr[i]-1]++;
-        //            }
-        //            printarray(arr);
-        //            printarray(counter);
-
-        //        }
-
+        //      printarray(newarr);
+        //}
 
         //static void task4()
         //{
-        //    int[] arr=new int[10];
-        //    int countp = 0;
-        //    int countn = 0;
-        //    int count0 = 0;
-        //    Random rnd=new Random();
-        //    for (int i = 0;i < arr.Length;i++)
+        //    int[] counter = new int[26];
+        //    char ch=char.Parse(Console.ReadLine());
+        //    while (ch!='.')
         //    {
-        //        arr[i] = rnd.Next(-11,11);
+        //        if(ch>'z' || ch<'a')
+        //            Console.WriteLine("invalid code");
+        //        else counter[ch-'a']++;  
+        //        ch = char.Parse(Console.ReadLine());
         //    }
+          
+        //    for(int i = 0;i<counter.Length;i++)
+        //    {
+        //        Console.WriteLine("for this char" + (char)(i + 'a') + ":" + counter[i]+"times");
+        //    }
+        //}  
+
+        //static void task6()
+        //{
+        //    int[] counter = new int[10];
+        //    int choice;
+        //    Console.WriteLine("please a number from 1-10 as you please");
+        //    choice =int.Parse(Console.ReadLine());
+        //    int maxvotes = counter[choice];
+        //    int maxind = choice;
+        //    counter[choice]++;
+        //    int numofworkers = 1;
+        //    while (choice!=0)
+        //    {
+        //        Console.WriteLine("please a number from 1-10 as you please,enter 0 to exit");
+        //        choice =int.Parse(Console.ReadLine());
+        //        counter[choice]++;
+        //        numofworkers++;
+        //        if (counter[choice]>maxvotes)
+        //        {
+        //            maxvotes=counter[choice];
+        //            maxind = choice;
+        //        }
+        //    }
+        //    printarray(counter);
+        //    Console.WriteLine("the max votes is "+maxvotes+" with th ind of "+maxind);
+        //}
+      
+
+        //static void nyfunc()
+//0 are in the end
+        //{
+        //    Random rnd = new Random();
+        //    int[] arr = new int[15];
+        //    int[] newArr = new int[arr.Length];
+        //    int j = 0;
+
         //    for (int i = 0; i < arr.Length; i++)
         //    {
-        //        if (arr[i] > 0)
-        //            countp++;
-        //        else if (arr[i] < 0) countn++;
-        //        else count0++;
+        //        arr[i] = rnd.Next(0, 6);
         //    }
-        //        printarray(arr);
-        //    Console.WriteLine(countp);
-        //    Console.WriteLine(countn);
-        //    Console.WriteLine(count0);
-        //}
 
-        //        static void task5()
-        ////fibunachi
-        //{
-        //    int[] arr = new int[20];
-        //    arr[0] = 1;
-        //    arr[1] = 1;
-        //    for(int i=2;i<arr.Length;i++)
-        //    {
-        //        arr[i] = arr[i - 2] + arr[i-1];
-        //    }
-        //}
-
-
-        //static void myfunc2()
-        //if the number exists you check it and tells him to try again
-        //{
-        //    int[] arr= new int[10]; 
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        Console.WriteLine("enter a full number");
-        //        int num = int.Parse(Console.ReadLine());
-        //        bool exists = false;
-        //        for (int j = 0; j < i; j++)
-        //        {
-        //            if (arr[j] == num)
-        //            {
-        //                exists = true;
-        //                break;
-        //            }
-        //        }
-        //        if (exists)
-        //        {
-        //            Console.WriteLine("Number already exists, try again.");
-        //            i--;
-        //        }
-        //        else arr[i] = num;
-        //    }
         //    printarray(arr);
-        //}
 
-//        static void myfunc3(int[] arr1, int[] arr2)
-//// in this function we take 2 of the arrays and take the numbers that are the same in both arrays
-//        {
-//            bool flag = false;
-//            int counter = 0;
-//            for (int i = 0;i< arr1.Length;i++)
-//            {
-//                flag = false;
-//                for(int j=0;j<arr2.Length;j++)
-//                {
-//                    if (arr1[i]==arr2[j])
-//                    {
-//                    flag = true;
-//                    break;
-//                    }
-                        
-//                }
-//                if (flag==true) counter++;    
-//            }
-//            int[] newarr= new int[counter];
-//            int index = 0;
-//            for (int i = 0; i < arr1.Length; i++)
-//            {
-//                flag = false;
-//                for (int j = 0; j < arr2.Length; j++)
-//                {
-//                    if (arr1[i] == arr2[j])
-//                    {
-//                        flag = true;
-//                        break;
-//                    }
+        //    // מעתיקים רק את הלא-אפסים
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] != 0)
+        //        {
+        //            newArr[j] = arr[i];
+        //            j++;
+        //        }
+        //    }
 
-//                }
-//                if (flag == true)
-//                    newarr[index++] = arr1[i];
-//            }
-//            printarray(newarr);
-//        }
-        
+        //    // השאר כבר 0 אוטומטית
+        //    printarray(newArr);
+        //  }
 
-
-        //static void myfunc4(int[] arr1, int[] arr2)
-        //in this function we take 2 of the arrays and take the numbers that are diffrent in both arrays
+        //static void myfunc()
+            //תוכנית זו בודקת אם ישנו ילד שהשם שלו כבר ב 8 שמות המצורפים לכיתה
         //{
-        //    bool flag = false;
-        //    int counter = 0;
-        //    for (int i = 0; i < arr1.Length; i++)
+        //    string[] names= new string[4];
+        //    for (int i = 0;i < names.Length-1;i++)
         //    {
-        //        flag = false;
-        //        for (int j = 0; j < arr2.Length; j++)
-        //        {
-        //            if (arr1[i] == arr2[j])
-        //            {
-        //                flag = true;
-        //                break;
-        //            }
-        //        }
-        //        if (flag != true) counter++;
+        //        Console.WriteLine("enter a name");
+        //        names[i] = Console.ReadLine();
         //    }
-        //    for (int i = 0; i < arr2.Length; i++)
+        //    Console.WriteLine("enter another name");
+        //    string name1= Console.ReadLine();
+        //    for (int i = 0; i < names.Length-1; i++)
         //    {
-        //        flag = false;
-        //        for (int j = 0; j < arr1.Length; j++)
+        //        if(names[i] != name1)
         //        {
-        //            if (arr2[i] == arr1[j])
-        //            {
-        //                flag = true;
-        //                break;
-        //            }
+        //            names[names.Length]=name1;
+        //            printarray(names);
         //        }
-        //        if (flag != true) counter++;
+                  
+        //        else Console.WriteLine("cannot join ");
+
         //    }
-        //    int[] newarr = new int[counter];
-        //    int index = 0;
-        //    for (int i = 0; i < arr1.Length; i++)
-        //    {
-        //        flag = false;
-        //        for (int j = 0; j < arr2.Length; j++)
-        //        {
-        //            if (arr1[i] == arr2[j])
-        //            {
-        //                flag = true;
-        //                break;
-        //            }
-        //        }
-        //        if (flag != true) newarr[index++] = arr1[i];
-        //    }
-        //    for (int i = 0; i < arr2.Length; i++)
-        //    {
-        //        flag = false;
-        //        for (int j = 0; j < arr1.Length; j++)
-        //        {
-        //            if (arr2[i] == arr1[j])
-        //            {
-        //                flag = true;
-        //                break;
-        //            }
-        //        }
-        //        if (flag != true) newarr[index++] = arr2[i];
-        //    }
-        //    printarray(newarr);
         //}
 
-        static void task8()
+        static void order()
         {
-            int[] arr=new int[10];
-            int counter1 = 0;
-            int counter2 = 0;
-            Random rnd = new Random();
-            for (int i = 0; i < arr.Length;i++)
+            int[] sum=new int[3];
+            bool flag = true;
+            Console.WriteLine("enter 1 for pizza ");
+            Console.WriteLine("enter 2 for pasta  ");
+            Console.WriteLine("enter 3 for salad  ");
+            int choice =int.Parse(Console.ReadLine());
+            Console.WriteLine("what is the amount of the food");
+            int amount = int.Parse(Console.ReadLine());
+            if(choice>0&&choice<4)
             {
-              arr[i] = rnd.Next(9,100);
+                if (choice == 1) sum[0] += amount;
+                else if(choice == 2) sum[1] += amount;
+                else sum[2] += amount;
             }
-            for (int i = 0;i < arr.Length-1;i++)
+            else Console.WriteLine("invalid code");
+            int max = sum[0];
+            int min = sum[0];
+            while (flag)
             {
-                if (arr[i] == arr[i+1])
-                    counter1++;
-                else if (arr[i] != arr[i + 1])
-                    counter2++;
-            }
-            printarray(arr);
-            if (counter1 == 10)
-                Console.WriteLine("this array numbers are all even");
-            if (counter2 == 10)
-                Console.WriteLine("this array numbers are all diffrent");
-            else Console.WriteLine("none of them");
-        }
+                Console.WriteLine("enter 1 for pizza enter 0 to exit ");
+                Console.WriteLine("enter 2 for pasta  ");
+                Console.WriteLine("enter 3 for salad  ");
+                choice = int.Parse(Console.ReadLine());
+                if (choice == 0)
+                    break;
+                Console.WriteLine("what is the amount of the food ");
+                amount = int.Parse(Console.ReadLine());
 
-        static void Main(string[] args)
+                if (choice > 0 && choice < 4)
+                {
+                    if (choice == 1) sum[0] += amount;
+                    else if (choice == 2) sum[1] += amount;
+                    else sum[2] += amount;
+                }
+            }
+                Console.WriteLine("pizza:"+ sum[0]);
+                Console.WriteLine("pasta:" + sum[1]);
+                Console.WriteLine("salad:" + sum[2]);
+                for (int i = 0; i < sum.Length; i++)
+                {
+                    if (sum[i] > max)
+                    {
+                         max = sum[i];
+
+                    }
+                    if (sum[i] < min)
+                    {
+                        min = sum[i];
+                    }
+                }
+                if (max == sum[0])
+                    Console.WriteLine("pizza max:" + sum[0]);
+                else if (max == sum[1])
+                    Console.WriteLine("pasta max:" + sum[1]);
+                else Console.WriteLine("salad max:" + sum[2]);
+
+                if (min == sum[0])
+                    Console.WriteLine("pizza min:" + sum[0]);
+                else if (min == sum[1])
+                    Console.WriteLine("pasta min:" + sum[1]);
+                else
+                    Console.WriteLine("salad min:" + sum[2]);
+
+            
+        }
+            static void Main(string[] args)
         {
-           
-            int[] arr1 = { 1, 2,6,9,2,8,10,11,12,13,14,15};
-            int[] arr2 = { 1, 6, 8, 2, 9, 0 };   
-           task8();
+            order();
+          
         }
     }
 }
